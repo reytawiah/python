d4e382f861fa69fef120eda524b9c33480d84e33
Reynolds Tawiah <tawiahreynolds@gmail.com>
2024-08-11 23:57:46 +0000

    Create numeric-data.py

---
+++ numeric-data.py
@@ -0,0 +1,55 @@
+# Python makes it easier to do math. In fact, Python is a popular language among data scientists, who must analyze large amounts of data. In this lab, you will explore the basic data types that are used to store numeric values.
+
+
+2 + 2
+
+4 - 2
+
+2 * 2
+
+4 / 2
+
+# Introducing the int data type
+
+print("Python has three numeric types: int, float, and complex")
+
+python3 <lab-python-file-name>.py
+
+# Creating a variable
+
+myValue=1
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Introducing the float data type
+myValue=3.14
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Introducing the complex data type
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
+# Introducing the bool data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
+
+
+
+
+
